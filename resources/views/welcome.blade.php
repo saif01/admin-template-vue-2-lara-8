347e52f12dd84bf38cd9465b14ddeb734d900fd8
Md. Syful Islam <syful.cse.bd@gmail.com>
2021-12-05 12:03:12 +0600

    link up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <link rel="stylesheet" href="{{ asset('./css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     </head>
     <body>
         <div id="app">
             <main-component></main-component>
         </div>
-        <script src="{{ asset('./js/app.js') }}"></script>
+        <script src="{{ asset('/js/app.js') }}"></script>
     </body>
 </html>
